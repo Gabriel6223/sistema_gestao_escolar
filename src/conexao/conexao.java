0706d2d3eca8ed81aeecbb0ff4d07d543e1a7898
Gabriel Dutra <gabriel62234556@alunos.sc.senac.br>
2026-06-22 21:29:23 -0300

    conexao

--- src/conexao/conexao.java
+++ src/conexao/conexao.java
@@ -1,13 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package conexao;
 
-/**
- *
- * @author erick62333106
- */
+package conexao;
+    import java.sql.Connection;
+    import java.sql.DriverManager;
+    import java.sql.SQLException;
+
 public class conexao {
+    
+    public static String URL = "jdbc:mysql://localhost:3306/sge";
+    public static String USUARIO="root";
+    public static String SENHA="root";
+    
+    public static Connection conectar(){
+        try{
+            Connection conn =
+                    DriverManager.getConnection(
+                            URL,
+                            USUARIO,
+                            SENHA
+                    );
+            System.out.println("Conectado!");
+            return conn;
+        }catch(SQLException e){
+            System.out.println("Erro na conexao");
+            
+            e.printStackTrace();
+            
+            return null;
+        }
+    }
     
 }
